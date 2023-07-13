a63221d6bee3d7036c659c6cc32ed32ca91e4993
양희연 <deepbm27@gmail.com>
2023-07-13 20:50:05 +0900

    Add: router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import NewProduct from './pages/NewProduct';
+import MyCart from './pages/MyCart';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/products', element: <Products /> },
+      { path: '/products/new', element: <NewProduct /> },
+      { path: '/products/:productId', element: <ProductDetail /> },
+      { path: '/carts', element: <MyCart /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
