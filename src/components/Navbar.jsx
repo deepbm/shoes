b27ae1cf8447cd57fd4a0b6f9146f53f78762c3c
양희연 <deepbm27@gmail.com>
2023-07-13 21:03:46 +0900

    Update: Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BsFillPencilFill } from 'react-icons/bs';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className='flex justify-between items-center'>
+      <Link to='/'>
+        <img className='w-32' src='/img/logo.svg' alt='logo' />
+      </Link>
+      <nav className='flex items-center gap-4'>
+        <Link to='/products'>Products</Link>
+        <Link to='/products/new'>
+          <BsFillPencilFill />
+        </Link>
+        <Link to='/carts'>Carts</Link>
+        <button>LogIn</button>
+      </nav>
+    </header>
+  );
 }
