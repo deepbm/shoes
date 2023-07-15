f4c7e4d07d005c42b6ffd9338a5d09fec6f63dfc
양희연 <deepbm27@gmail.com>
2023-07-15 18:09:58 +0900

    Add: admin user

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,22 +6,25 @@ import {
   signInWithPopup,
   signOut,
 } from 'firebase/auth';
+import { get, getDatabase, ref } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const db = getDatabase(app);
 
 export async function login() {
   return signInWithPopup(auth, provider)
     .then(result => {
       const user = result.user;
-      return user;
+      return adminUser(user);
     })
     .catch(console.error);
 }
@@ -31,9 +34,19 @@ export async function logout() {
 }
 
 export async function onUserStateChange(callback) {
-  onAuthStateChanged(auth, user => {
-    if (user) {
-      callback(user);
-    }
+  onAuthStateChanged(auth, async user => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
   });
+}
+
+export async function adminUser(user) {
+  return get(ref(db, 'admins')) //
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        return { ...user, isAdmin: admins.includes(user.uid) };
+      }
+      return user;
+    });
 }
