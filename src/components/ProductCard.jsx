077a6ab9bd7b5d387835b5a8d92fc000d3a16d65
양희연 <deepbm27@gmail.com>
2023-07-16 13:04:02 +0900

    Add: ProductDetail component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function ProductCard({ product: { title, price, image } }) {
+export default function ProductCard({ product, product: { id, title, price, image } }) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/products/${id}`, { state: { product } });
+  };
+
   return (
-    <li className='rounded-lg overflow-hidden shadow-md cursor-pointer'>
+    <li
+      className='rounded-lg overflow-hidden shadow-md cursor-pointer transition-all hover:scale-105'
+      onClick={handleClick}
+    >
       <img className='w-full' src={image} alt={title} />
       <div className='p-2'>
         <h3>{title}</h3>
