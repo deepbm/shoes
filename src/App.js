e6213b9caf673fb2ffdfef5aa4dad2b388315766
양희연 <deepbm27@gmail.com>
2023-08-04 13:09:45 +0900

    Add: Footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Navbar from './components/Navbar';
 import { UserProvider } from './contexts/UserContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Footer from './components/Footer';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <Navbar />
         <Outlet />
+        <Footer />
       </QueryClientProvider>
     </UserProvider>
   );
