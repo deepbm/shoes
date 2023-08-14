b4855912e0d15bdd4a383815548c153a030daa31
양희연 <deepbm27@gmail.com>
2023-08-14 14:36:21 +0900

    Refactor: MyCart component

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import CartItem from '../components/CartItem';
 import useCarts from '../hooks/useCarts';
-import Button from '../components/ui/Button';
 import { Link } from 'react-router-dom';
 
 const DELIVERY_FEE = 3000;
